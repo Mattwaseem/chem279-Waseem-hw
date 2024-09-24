a06c968264591ec5b364b004321de116a46b34f3
Matt Waseem <109879665+Mattwaseem@users.noreply.github.com>
2024-09-23 18:41:06 -0700

    fixed the script in main to calculate turnication error and plotting script added

--- build/CMakeFiles/clean-bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean-bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean-bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
